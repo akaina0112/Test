52a23c9b8ffba8a2f5f032e6bf25ee5f049c521f
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-04-28 05:29:22 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-// server.js
+// config.js
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const PORT = 3000;
+const config = {
+    owner: 'owner_name', // GitHubリポジトリの所有者名
+    repo: 'repository_name' // GitHubリポジトリの名前
+};
 
-let downloadCount = 0; // 仮のダウンロード数
-
-// JSON形式のデータを解析するためのミドルウェアを追加
-app.use(bodyParser.json());
-
-// ダウンロード数を取得するエンドポイント
-app.get('/api/downloads', (req, res) => {
-    res.json({ downloads: downloadCount });
-});
-
-// ダウンロード数をインクリメントするエンドポイント
-app.post('/api/downloads', (req, res) => {
-    if (req.body.increment) {
-        downloadCount++; // ダウンロード数をインクリメント
-    }
-    res.json({ downloads: downloadCount });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export default config;
