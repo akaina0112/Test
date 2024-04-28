212cac9e5ff82513eceeeab468a4c17cb713b0ee
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-04-28 14:50:43 +0900

    Update githubConfig.js

--- githubConfig.js
+++ githubConfig.js
@@ -1,6 +1,45 @@
-const githubConfig = {
-    owner: 'akaina0112',
-    repo: 'Test'
-};
+import { owner, repo, fileNames, set } from './config.js';
 
-export default githubConfig;
+function fetchFileData(fileName) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://api.github.com/repos/${owner}/${repo}/releases`)
+            .then(response => response.json())
+            .then(data => {
+                let downloadCount = 0;
+                let downloadUrl = '';
+                data.forEach(release => {
+                    release.assets.forEach(asset => {
+                        if (asset.name === fileName) {
+                            downloadCount += asset.download_count;
+                            downloadUrl = asset.browser_download_url;
+                        }
+                    });
+                });
+                const fileData = {
+                    fileName: fileName,
+                    downloadCount: downloadCount,
+                    downloadUrl: downloadUrl
+                };
+                resolve(fileData);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function fetchReleaseInfo() {
+    return Promise.all(fileNames.map(fileName => fetchFileData(fileName)))
+        .then(fileDataArray => {
+            return fileDataArray;
+        })
+        .catch(error => {
+            console.error('Error fetching file info:', error);
+        });
+}
+
+function downloadAsset(url) {
+    window.open(url, '_blank');
+}
+
+export { fetchReleaseInfo, downloadAsset };
