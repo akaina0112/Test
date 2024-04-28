b4de1a84df45003efb5a7bba94ee0ad92925b4ef
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-04-28 10:43:14 +0900

    Update and rename fileConfig.js to githubAndFileConfig.js

--- fileConfig.js
+++
@@ -1,8 +0,0 @@
-const fileConfig = {
-    fileName1: '20230704_185437.jpg',
-    fileName2: '20230704_185437.jpg',
-    fileName3: '20230704_185437.jpg',
-    fileName4: '20230704_185437.jpg'
-};
-
-export default fileConfig;
---
+++ githubAndFileConfig.js
@@ -0,0 +1,15 @@
+// githubAndFileConfig.js
+
+const githubConfig = {
+    owner: 'akaina0112',
+    repo: 'Test'
+};
+
+const fileConfig = [
+    '20230704_185437.jpg',
+    'example.zip',
+    'sample.txt',
+    'document.pdf'
+];
+
+export { githubConfig, fileConfig };
