232d5d60c5d590521ece325c2fb3178435190b0b
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-04-28 10:21:47 +0900

    Update fileConfig.js

--- fileConfig.js
+++ fileConfig.js
@@ -1,7 +1,20 @@
-// fileConfig.js
+const fileConfig = [
+    {
+        fileName: '20230704_185437.jpg',
+        index: 1
+    },
+    {
+        fileName: '20230704_185437.jpg',
+        index: 2
+    },
+    {
+        fileName: '20230704_185437.jpg',
+        index: 3
+    },
+    {
+        fileName: '20230704_185437.jpg',
+        index: 4
+    }
+];
 
-const fileConfig = {
-    fileName1: '20230704_185437.jpg', // ファイル1の名前
-    fileName2: 'example.zip' // ファイル2の名前
-};
-
+export default fileConfig;
