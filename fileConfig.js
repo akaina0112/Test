7024c0028855501e375a87708704cebe1734ce97
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-04-28 10:27:46 +0900

    Update fileConfig.js

--- fileConfig.js
+++ fileConfig.js
@@ -1,10 +1,8 @@
 const fileConfig = {
-    files: [
-        '20230704_185437.jpg',
-        'example.zip',
-        'sample.txt',
-        'document.pdf'
-    ]
+    fileName1: '20230704_185437.jpg',
+    fileName2: 'example.zip',
+    fileName3: 'sample.txt',
+    fileName4: 'document.pdf'
 };
 
 export default fileConfig;
