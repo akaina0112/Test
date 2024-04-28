8f72cf03a880b1576ebd3fef62dfe423f9efd2bb
ライフガード <162636336+akaina0112@users.noreply.github.com>
2024-04-28 14:51:16 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const owner = 'akaina0112'; // GitHubリポジトリの所有者名
+const repo = 'Test'; // GitHubリポジトリの名前
+const fileNames = ['20230704_185437.jpg', 'example.zip', 'sample.txt', 'document.pdf', 'data.csv']; // ファイル名の配列
+const set = ' DL'; // 名前
+
+export { owner, repo, fileNames, set };
